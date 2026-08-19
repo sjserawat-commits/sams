b349bb7dd257168d67ae9b50ecd752b492b4999f
Suraj <sjserawat07@gmail.com>
2026-08-19 22:53:43 +0530

    remove incorrect editable investigation review page

--- app/investigations/orders-editable/page.tsx
+++
@@ -1,3 +0,0 @@
-"use client";
-import { useEffect } from "react";
-export default function RemovedEditableInvestigationOrder(){useEffect(()=>{window.location.replace("/investigations/orders");},[]);return null;}
